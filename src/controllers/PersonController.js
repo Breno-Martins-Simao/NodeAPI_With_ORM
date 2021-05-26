4134b5c843f1184f1334f4b69fc0138828362ac0
Breno Martins <brenomsimao123@outlook.com>
2021-05-25 21:17:44 -0300

    Added enrollments controller in people controller

--- src/controllers/PersonController.js
+++ src/controllers/PersonController.js
@@ -50,7 +50,77 @@ class PersonController {
         } catch(e){
             return response.status(500).json(e.message)
         }
-    
+    }
+
+    static async deletePerson(request, response){
+        const {id} = request.params
+        try{
+            await database.People.destroy({where:{
+                id:Number(id)
+            }})
+            return response.status(200).json({message:`ID <${id}> Deleted sucessfully`})
+        }catch(error){
+            return response.status(500).json(error.message)
+        }
+    }
+
+    static async findEnrollment (request, response){
+        const { student_id, enrollment_Id } = request.params
+        try {
+            const enrollment = await database.Enrollment.findOne({
+                where:{
+                    student_id:Number(student_id),
+                    id:Number(enrollment_Id)
+                }
+            })
+            return response.status(200).json(enrollment)
+        } catch(e){
+            return response.status(500).json(e.message)
+        }
+    } 
+
+    static async createEnrollment (request, response){
+        const { student_id } = request.params
+        const  newEnrollment = {...request.body, student_id : Number(student_id)}
+        try {
+            const createdEnrollment = await database.Enrollment.create(newEnrollment)
+            return response.status(200).json(createdEnrollment)
+        } catch(e){
+            return response.status(500).json(e.message)
+        }
+    }
+
+    static async updateEnrollment (request, response){
+        const { student_id, enrollment_Id } = request.params
+        const updtEnrollment = request.body
+        try {
+            await database.Enrollment.update(updtEnrollment,{ where: { 
+                id: Number(enrollment_Id),
+                student_id: Number(student_id)
+            }})
+            
+            const updatedEnrollment= await database.Enrollment.findOne({
+                where:{
+                    id:Number(enrollment_Id)
+                }
+            })
+            response.status(200).json(updatedEnrollment)
+        } catch(e){
+            return response.status(500).json(e.message)
+        }
+    }
+
+    static async deleteEnrollment(request, response){
+        const { student_id, enrollment_Id } = request.params
+        try{
+            await database.Enrollment.destroy({where:{
+                id:Number(enrollment_Id),
+                student_id:Number(student_id)
+            }})
+            return response.status(200).json({message:`Enrollment ID <${enrollment_Id}> that includes stundet_id <${student_id}> Deleted sucessfully`})
+        }catch(error){
+            return response.status(500).json(error.message)
+        }
     }
 }
 
--- src/routes/peopleRoute.js
+++ src/routes/peopleRoute.js
@@ -1,11 +1,18 @@
 const { Router }  = require('express')
-const PessoaController = require('../controllers/PersonController')
+const PersonController = require('../controllers/PersonController')
 
 const router = Router()
 
-router.get('/person', PessoaController.showAllPeople)
-router.get('/person/:id', PessoaController.findPerson)
-router.post('/person', PessoaController.createPerson)
-router.put('/person/:id', PessoaController.updatePerson)
+router.get('/person', PersonController.showAllPeople)
+router.get('/person/:id', PersonController.findPerson)
+router.post('/person', PersonController.createPerson)
+router.put('/person/:id', PersonController.updatePerson)
+router.delete('/person/:id', PersonController.deletePerson)
+
+
+router.get('/person/:student_id/enrollment/:enrollment_Id', PersonController.findEnrollment)
+router.post('/person/:student_id/enrollment', PersonController.createEnrollment)
+router.put('/person/:student_id/enrollment/:enrollment_Id', PersonController.updateEnrollment)
+router.delete('/person/:student_id/enrollment/:enrollment_Id', PersonController.deleteEnrollment)
 
 module.exports = router
